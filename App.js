cfa7c04abcc0cfe69a5297e2f5f33d5f54a89fac
henrique.roos <henrique.roos@egadnet.com.br>
2023-12-28 16:53:53 -0300

    feat: add custom colors

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StatusBar } from "expo-status-bar";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./UI/IconButton";
+import { Colors } from "./contants/colors";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,11 +13,20 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: {
+              backgroundColor: Colors.gray700,
+            },
+          }}
+        >
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
             options={({ navigation }) => ({
+              title: "Your Favorite Places",
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
@@ -27,7 +37,11 @@ export default function App() {
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{ title: "Add a new Place" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
